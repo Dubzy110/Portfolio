212e84943ed93bac5880de1733f46dc131558f79
Dubzy110 <Dubzy110@gmail.com>
2025-02-13 16:04:29 -0600

    removed extra files and made routing foundation

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="text-green-500 text-3xl underline font-bold">test</div>
+    </>
+  );
+};
+
+export default About;
